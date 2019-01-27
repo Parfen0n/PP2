6d8d7f376ed95c8264a6f9f00dc23f00bc8330e4
Parfen0n <parfenon666@gmail.com>
2019-01-27 19:28:42 +0600

    Some features in task2 + comments

--- Week1/Task2/Program.cs
+++ Week1/Task2/Program.cs
@@ -10,12 +10,13 @@ namespace Task2
     {
         public string name;
         public string id;
-        public int year = 1;
+        public int year;
 
-        public Student(string name, string id)
+        public Student(string name, string id, int year)
         {
             this.name = name; // Назначение классу Student имени введенного пользозователем
             this.id = id; // Назначение классу Student id введенного пользователем
+            this.year = year; // Назначение классу Student year введенного пользователем
         }
 
 
@@ -23,8 +24,14 @@ namespace Task2
         {
             Console.WriteLine("Имя студента: {0}", name); // Вывод имени студента
             Console.WriteLine("ID студента: {0}", id); // Вывод ID студента
-            year++; // Увелечение года обучения на 1 (инкремент)
-            Console.WriteLine("Через год вы будете на {0} курсе", year); // Вывод курса обучающегосяя через год
+            if(year == 4) // Проверка года обучения
+            {
+                Console.WriteLine("Вы выпускник! На следующий год вы можете поступить на магистратуру :)");
+            } else
+            {
+                year++; // Увелечение года обучения на 1 (инкремент)
+                Console.WriteLine("Через год вы будете на {0} курсе", year); // Вывод курса обучающегосяя через год
+            } 
         }
 
     }
@@ -33,8 +40,8 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите своё имя и ID:"); // Вопрос к пользователю
-            Student st1 = new Student(Console.ReadLine(), Console.ReadLine()); // Ввод данных
+            Console.WriteLine("Введите своё имя, ID и год обучения:"); // Вопрос к пользователю
+            Student st1 = new Student(Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine())); // Ввод данных
             st1.print(); // Вывод данных
             Console.ReadLine(); // Костыль, для того чтобы терминал не закрывался
         }

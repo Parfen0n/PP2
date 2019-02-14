915def9881cc0199a58427b839e0aa371c2ce3ca
Parfen0n <parfenon666@gmail.com>
2019-02-14 19:05:17 +0600

    Task4

---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\My\Study at Uni\PP2\Week2\Path\Example.txt";
+            string path1 = @"C:\My\Study at Uni\PP2\Week2\Path1";
+
+            DirectoryInfo directInfo = new DirectoryInfo(path1);
+            FileInfo file = new FileInfo(path);
+            if(file.Exists && directInfo.Exists)
+            {
+                file.CopyTo(path1 + @"\Copied.txt", true);
+                file.Delete();
+            }
+            else
+            {
+                Console.WriteLine("Данный путь не найден");
+            }
+        }
+    }
+}
